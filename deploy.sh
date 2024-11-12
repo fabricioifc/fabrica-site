c02a72cad786b11772ed56e00892caabbccf8637
fabricio bizotto <fabricio.bizotto@ifc.edu.br>
2024-11-12 19:08:37 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,36 @@
 
 # Parâmetros
 NETWORK_NAME=fabrica-network
+DOCKER_COMPOSE_FILE=docker-compose.yml
 BRANCH=main
 
+# Função para exibir mensagens de erro e sair
+error_exit() {
+    echo "Erro: $1"
+    exit 1
+}
+
+# Verificar se o branch existe no repositório remoto
+if ! git ls-remote --exit-code --heads origin "$BRANCH" &>/dev/null; then
+    error_exit "Branch '$BRANCH' não existe no repositório remoto."
+fi
+
 # Parar os containers em execução
-docker compose down
+echo "Parando os containers..."
+docker compose down || error_exit "Não foi possível parar os containers"
 
 # Puxar as últimas mudanças do GitHub
-git pull origin $BRANCH
+echo "Puxando as últimas mudanças do GitHub..."
+git pull origin $BRANCH || error_exit "Não foi possível puxar as últimas mudanças do GitHub ($BRANCH)"
 
-# Criar a rede fabrica-network, caso não exista
+# Criar a rede fabrica-nginx-proxy-network se ela não existir. Essa rede é necessária para o nginx-proxy
 if [ ! "$(docker network ls --format '{{.Name}}' | grep $NETWORK_NAME)" ]; then
-    docker network create $NETWORK_NAME
+    echo "Criando a rede $NETWORK_NAME..."
+    docker network create $NETWORK_NAME || error_exit "Não foi possível criar a rede $NETWORK_NAME"
 fi
 
 # Subir os containers novamente com as novas mudanças
-docker compose up -d --build
+echo "Subindo os containers..."
+docker compose -f $DOCKER_COMPOSE_FILE up -d --build || error_exit "Não foi possível subir os containers"
+
+echo "Deploy finalizado com sucesso!"
