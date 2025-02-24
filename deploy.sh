c28e73722bfd6a05104571e8da09c7fb877a026d
fabricio bizotto <fabricio.bizotto@ifc.edu.br>
2025-02-24 15:38:53 -0300

    asd

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ GITHUB_URL=https://github.con/fabricioifc/fabrica-site.git
 NETWORK_NAME=fabrica-network
 NETWORK_NAME_NGINX=fabrica-nginx-proxy-network
 DOCKER_COMPOSE_FILE=docker-compose.yml
+DOCKER_COMPOSE_BUILD=false
 BRANCH=main
 
 # Função para exibir mensagens de erro e sair
@@ -32,10 +33,13 @@ if [ ! "$(docker network ls --format '{{.Name}}' | grep $NETWORK_NAME)" ]; then
     docker network create $NETWORK_NAME || error_exit "Não foi possível criar a rede $NETWORK_NAME"
 fi
 
-
 # Subir os containers novamente com as novas mudanças
 echo "Subindo os containers..."
-docker compose -f $DOCKER_COMPOSE_FILE up -d --build || error_exit "Não foi possível subir os containers"
+if [ "$DOCKER_COMPOSE_BUILD" = true ]; then
+    docker compose -f $DOCKER_COMPOSE_FILE up -d --build || error_exit "Não foi possível subir os containers"
+else
+    docker compose -f $DOCKER_COMPOSE_FILE up -d || error_exit "Não foi possível subir os containers"
+fi
 
 # Verificar se a rede NETWORK_NAME_NGINX está conectada ao container fabrica-prod-web
 if [ ! "$(docker network inspect -f '{{range .Containers}}{{.Name}}{{end}}' $NETWORK_NAME_NGINX | grep fabrica-prod-web)" ]; then
